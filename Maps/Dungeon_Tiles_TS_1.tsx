9b8edea6496a5d79270e428a357fff304ca45f59
Julia McKay <juliamckay@ufl.edu>
2022-07-02 20:01:59 +0900

    start button now opens lvl 1 map. title txt off center due to larger screen size

---
+++ Maps/Dungeon_Tiles_TS_1.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Dungeon_Tiles" tilewidth="16" tileheight="16" tilecount="42" columns="7">
+ <image source="Dungeon_Tiles_01.png" trans="aaffff" width="112" height="96"/>
+</tileset>
--- game.py
+++ game.py
@@ -1,6 +1,6 @@
 import arcade
 import arcade.gui
-from constants import SCREEN_WIDTH, SCREEN_HEIGHT
+from constants import SCREEN_WIDTH, SCREEN_HEIGHT, TILE_SCALING
 
 
 class GameScreen(arcade.View):
@@ -11,9 +11,28 @@ class GameScreen(arcade.View):
         self.manager = arcade.gui.UIManager()
         self.manager.enable()
 
+        #game screen
+        self.tile_map = None
+        self.scene = None
+
+    def setup(self):
+        """Set up the game here. Call this function to restart the game."""
+        # name of map to load
+        map_name = "Maps/Level_0_map.json"
+        layer_options = {
+            "Platforms": {
+                "use_spatial_hash": True,
+            },
+        }
+
+        self.tile_map = arcade.load_tilemap(map_name, TILE_SCALING, layer_options)
+        self.scene = arcade.Scene.from_tilemap(self.tile_map)
+
     def on_show_view(self):
-        arcade.set_background_color(arcade.color.WHITE)
+        self.setup()
+        arcade.set_background_color(arcade.color.GRAY)
 
     def on_draw(self):
         self.clear()
         self.manager.draw()
+        self.scene.draw()
