f36c887edec8a8fc998943f6a568018b4b94bd8d
Julia McKay <juliamckay@ufl.edu>
2022-07-27 23:03:46 +0900

    replaced background for level 1, added decorations

---
+++ src/Assets/Maps/Dungeon_Tiles_BG.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="Dungeon_Tiles_BG" tilewidth="16" tileheight="16" tilecount="81" columns="9">
+ <image source="dungeon_tiles_BG.png" width="144" height="144"/>
+</tileset>
---
+++ src/Assets/Maps/banner.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.9" tiledversion="1.9.0" name="banner" tilewidth="16" tileheight="32" tilecount="1" columns="1">
+ <image source="banner.png" width="16" height="32"/>
+</tileset>
